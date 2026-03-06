87fa85c0bec45467a6344455082c58fe5699c9b8
rhettholmes3 <rhettholmes3@gmail.com>
2026-03-06 17:13:31 +0800

    Initial commit for Voice Gateway Plugin (Phase 1-5)

--- dist/http/call-api.d.ts
+++
@@ -1,6 +0,0 @@
-import { CallManager } from '../call/call-manager';
-import type { ZegoConfig } from '../types/config';
-export declare function startCallHandler(manager: CallManager, config: ZegoConfig): (req: any, res: any) => Promise<any>;
-export declare function endCallHandler(manager: CallManager): (req: any, res: any) => Promise<any>;
-export declare function statusHandler(manager: CallManager): (req: any, res: any) => Promise<any>;
-export declare function refreshTokenHandler(manager: CallManager, config: ZegoConfig): (req: any, res: any) => Promise<any>;
